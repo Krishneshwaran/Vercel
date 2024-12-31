03313efcd591bfe9b3f52e8979002ce1b1e81e96
Krishneshwaran <krishneshwarand@gmail.com>
2024-12-31 14:39:02 +0530

    Fixing Production Bug2

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%1l2^d$2q8xuwtmv-z=_!&529loppvl)ikcs0&ef=safzcn=uw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', 'https://portal-sigma-lac.vercel.app/' ]
 
 
 # Application definition
